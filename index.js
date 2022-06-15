856e788226b63d005bf0db56c36493c3fe25f881
Jianqiu Wang <jianqiuwang1@icloud.com>
2022-06-15 15:38:58 -0500

    commit tree 19ffd88fe9ff11179bf103c3cd7d157778a36409

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(namesArray, event){
+    let cards = []
+    for(let i=0; i<namesArray.length; i++){
+        cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+
+    }
+    return cards
+}
+
+function countDown(number){
+    while(number>=0){
+        console.log(number)
+        number--
+    }
+
+}
+
